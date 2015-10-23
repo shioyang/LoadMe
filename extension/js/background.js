ac813b8f467a7077afdb87c4624314056f11b80c
shioyang <shioyang4321@gmail.com>
2015-10-23 23:08:38 +0900

    Retrieve changed filenames

--- extension/js/background.js
+++ extension/js/background.js
@@ -5,6 +5,9 @@
 			console.log("[background] " + msg);
 		};
 
+		this.changedFilenames = [];
+		var t = this;
+
 		var socket = io('http://localhost:8888');
 		socket.on('connect', function() {
 			console.log("connect!");
@@ -18,7 +21,21 @@
 
 		// when 'changed' received:
 		socket.on('changed', function(data) {
-			console.log(data);
+			t.logDebug(data);
+			// ChangedInfo
+			//    {
+			//       filenames: [
+			//          "sample.txt"
+			//       ]
+			//    }
+			var changedInfo = JSON.parse(data);
+			var filenames = changedInfo.filenames;
+			filenames.forEach(function(filename, index) {
+				// Add the filename if not exist in changedFilenames
+				if (this.changedFilenames.indexOf(filename) < 0)
+					this.changedFilenames.push(filename);
+				this.logDebug(this.changedFilenames);
+			}, t);
 		});
 
 	};
