cf500022fc80d44274c0b517883a114eca724674
shioyang <shioyang4321@gmail.com>
2015-10-24 20:51:26 +0900

    Remove listener when disconnect

--- extension/js/background.js
+++ extension/js/background.js
@@ -8,15 +8,50 @@
 		this.changedFilenames = [];
 		var t = this;
 
+		this.rewriteHeader = function(details) {
+			if (details.url && details.url.includes("google")) {
+				console.log("===");
+				console.log(details.url);
+				console.log(details.requestHeaders);
+				var foundCacheControl = false;
+				var foundPragma = false;
+				details.requestHeaders.forEach(function(header, index) {
+					console.log(header);
+					if (header.name && header.name === "Cache-Control") {
+						foundCacheControl = true;
+						header.value = "no-cache";
+					}
+					if (header.name && header.name === "Pragma") {
+						foundPragma = true;
+						header.value = "no-cache";
+					}
+				}, t);
+				if (!foundCacheControl)
+					details.requestHeaders.push({name: "Cache-Control", value: "no-cache"});
+				if (!foundPragma)
+					details.requestHeaders.push({name: "Pragma", value: "no-cache"});
+				console.log(details.requestHeaders);
+			}
+			return {requestHeaders: details.requestHeaders};
+		};
+
 		var socket = io('http://localhost:8888');
 		socket.on('connect', function() {
 			console.log("connect!");
+			chrome.webRequest.onBeforeSendHeaders.addListener(
+				t.rewriteHeader,
+				{urls: ["*://www.google.co.jp/images/*"]},
+//				{urls: ["<all_urls>"]},
+				["requestHeaders"]
+			);
 		});
+
 		socket.on('greeting', function(data) {
 			console.log(data);
 		});
 		socket.on('disconnect', function() {
 			console.log("disconnect!");
+			chrome.webRequest.onBeforeSendHeaders.removeListener( t.rewriteHeader );
 		});
 
 		// when 'changed' received:
@@ -37,30 +72,6 @@
 				this.logDebug(this.changedFilenames);
 			}, t);
 		});
-
-		chrome.webRequest.onBeforeSendHeaders.addListener(
-			function(details) {
-				if (details.url && details.url.includes("google")) {
-					console.log("===");
-					console.log(details.url);
-					console.log(details.requestHeaders);
-					var found = false;
-					details.requestHeaders.forEach(function(header, index) {
-						console.log(header);
-						if (header.name && header.name === "Cache-Control") {
-							found = true;
-							header.value = "no-cache";
-						}
-					}, t);
-					if (!found)
-						details.requestHeaders.push({name: "Cache-Control", value: "no-cache"});
-					console.log(details.requestHeaders);
-				}
-				return {requestHeaders: details.requestHeaders};
-			},
-			{urls: ["<all_urls>"]},
-			["requestHeaders"]
-		);
 
 	};
 
